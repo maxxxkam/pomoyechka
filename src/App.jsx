45fd36cefaa6d2d81487354bfb8cf54c2f8653fe
maxxxkam <muslimovmaxkam@gmail.com>
2024-10-18 19:02:56 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Card from './components/Card/Card'
+import './App.css'
+
+
+const App = () => {
+  return (
+    <>
+  <div className="wrap">
+    <Card />
+  </div>
+
+
+    </>
+  )
+}
+
+export default App
